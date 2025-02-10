759693c89d72932b84cba1625d2688579d647266
William <williamcranneyfee@outlook.com>
2025-02-10 10:40:57 -0800

    wrote fit_and_plot_T1 function for Pulsed NMR.py. It doesn't work yet

--- Pulsed NMR.py
+++ Pulsed NMR.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.signal as sig
+from fontTools.ttx import parseOptions
 from scipy.optimize import curve_fit
 from SDOM_analysis import SDOM_analysis
 from linear_regression import linear_regression
@@ -12,10 +13,27 @@ mu_N = scipy.constants.physical_constants['nuclear magneton'][0]
 h = scipy.constants.h
 
 
-def exponentialFunc(x,a,b):
-    y = np.exp(a*x) + b
+def exponentialFunc(x,a,b,c,d):
+    y = a*np.exp(b*x + c) + d
     return y
 
-def fit_and_plot_T1(filepath):
+
+def fit_and_plot_T1(filepath, fignum):
     df = pd.read_csv(filepath)
-    popt, pcov = curve_fit(exponentialFunc, df[''])
+    fit = curve_fit(exponentialFunc, df['Delay Time (s)'], df['Voltage (V)'], p0=[-41, 0.5, -0.5, 12],
+                    check_finite=True)
+
+    popt, pcov = fit[0], fit[1]
+
+    x_arr = np.linspace(df['Delay Time (s)'].min(), df['Delay Time (s)'].max(), 1000)
+    y_arr = exponentialFunc(x_arr, *popt)
+    plt.plot(x_arr, y_arr)
+    plt.scatter(df['Delay Time (s)'].values, df['Voltage (V)'].values, color='red')
+    plt.xlabel('Delay Time (s)')
+    plt.ylabel('Voltage (V)')
+    plt.show()
+
+    return y_arr
+
+fit_and_plot_T1('Data/Pulsed NMR Data/Water/WaterT1.csv', 1)
+plt.show()
